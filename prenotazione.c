5fd6ecbaa444ca5abc57ffed741aa7a0ee05dad4
Bistroh <148788324+Bistroh@users.noreply.github.com>
2025-05-13 17:42:21 +0200

    Source+Header Prenotazione
    struttura prenotazione e funzioni per gestione di tali create

---
+++ prenotazione.c
@@ -0,0 +1,70 @@
+//
+// Created by extra on 30/04/2025.
+//
+
+#include "prenotazione.h"
+#include "Auto.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int giorno;
+    int mese;
+    int anno;
+    int oraInizio;
+    int oraFine;
+} data;
+
+struct s_prenotazione {
+    int ID_prenotazione;
+    char CF[17];
+    char targa[10];
+    data dataPrenotazione;
+};
+
+Prenotazione creaPrenotazione(int id, char *CF, char *targa, int giorno, int mese, int anno, int oraInizio, int oraFine) {
+    Prenotazione nuovaPrenotazione = malloc(sizeof(struct s_prenotazione));
+    if (nuovaPrenotazione == NULL) {
+        fprintf(stderr, "Errore di allocazione memoria\n");
+        exit(EXIT_FAILURE);
+    }
+    nuovaPrenotazione->ID_prenotazione = id; // Inizializza l'ID della prenotazione
+    strncpy(nuovaPrenotazione->CF, CF, sizeof(nuovaPrenotazione->CF) - 1);
+    nuovaPrenotazione->CF[sizeof(nuovaPrenotazione->CF) - 1] = '\0'; // Assicurati che la stringa sia terminata
+    strncpy(nuovaPrenotazione->targa, targa, sizeof(nuovaPrenotazione->targa) - 1);
+    nuovaPrenotazione->targa[sizeof(nuovaPrenotazione->targa) - 1] = '\0';
+    nuovaPrenotazione->dataPrenotazione.giorno = giorno;
+    nuovaPrenotazione->dataPrenotazione.mese = mese;
+    nuovaPrenotazione->dataPrenotazione.anno = anno;
+    nuovaPrenotazione->dataPrenotazione.oraInizio = oraInizio;
+    nuovaPrenotazione->dataPrenotazione.oraFine = oraFine;
+    return nuovaPrenotazione;
+}
+
+int getIDPrenotazione(Prenotazione prenotazione) {
+    return prenotazione->ID_prenotazione;
+}
+
+char *getCFPrenotazione(Prenotazione prenotazione) {
+    return prenotazione->CF;
+}
+
+char *getTargaPrenotazione(Prenotazione prenotazione) {
+    return prenotazione->targa;
+}
+
+char *getDataPrenotazione(Prenotazione prenotazione) {
+    static char dataStr[20];
+    snprintf(dataStr, sizeof(dataStr), "%02d/%02d/%04d %02d-%02d", prenotazione->dataPrenotazione.giorno,
+             prenotazione->dataPrenotazione.mese, prenotazione->dataPrenotazione.anno,
+             prenotazione->dataPrenotazione.oraInizio, prenotazione->dataPrenotazione.oraFine);
+    return dataStr;
+}
+
+void stampaPrenotazione(Prenotazione prenotazione) {
+    printf("ID Prenotazione: %d\n", prenotazione->ID_prenotazione);
+    printf("CF: %s\n", prenotazione->CF);
+    printf("Targa: %s\n", prenotazione->targa);
+    printf("Data Prenotazione: %s\n", getDataPrenotazione(prenotazione));
+}
---
+++ prenotazione.h
@@ -0,0 +1,18 @@
+//
+// Created by extra on 30/04/2025.
+//
+
+#ifndef PRENOTAZIONE_H
+#define PRENOTAZIONE_H
+
+typedef struct s_prenotazione *Prenotazione;
+
+Prenotazione creaPrenotazione(int id, char *CF, char *targa, int giorno, int mese, int anno, int oraInizio, int oraFine);
+int getIDPrenotazione(Prenotazione prenotazione);
+char *getCFPrenotazione(Prenotazione prenotazione);
+char *getTargaPrenotazione(Prenotazione prenotazione);
+char *getDataPrenotazione(Prenotazione prenotazione);
+void stampaPrenotazione(Prenotazione prenotazione);
+
+
+#endif //PRENOTAZIONE_H
