09e92f0936e3bb09ca6a687b1a534757f49cc3da
G-Sarlo <sarlo.giuseppe@gmail.com>
2025-05-18 15:37:35 +0200

    Aggiornamento main.c
    Aggiornata la gestione delle variabili `scelta` con l'introduzione di `sceltaMenu`. Inizializzate le HashTable di Utenti e Auto, inizializzata la lista prenotazioni e aggiunto il messaggio finale all'uscita dal menu.

--- main.c
+++ main.c
@@ -6,36 +6,44 @@
 #include "Utile_DevMenu.h"
 #include "HashTbUtenti.h"
 #include "HashTbAuto.h"
+#include "List_Prenotazione.h"
 
 
 
 int main() {
-    int scelta = selezionaRuolo(); // 1 per utente, 2 per sviluppatore
-
     // Con UTHash inizializziamo a NULL
     HashTable tabUtenti = newHashtableUtenti();
     AutoHashTable tabAuto = creaHashTableAuto();
+    List listaPrenotazioni = newList();
+    int sceltaMenu = 0;
+    int scelta = 0;
 
-    Utente u;
+    do{
+        scelta = selezionaRuolo(); // 1 per utente, 2 per sviluppatore
 
-    if (scelta == 1) {
-        u = loginRegisterUtente(&tabUtenti);
-        if (u != NULL) {
-            do {
-                scelta = menuUtente();
-                switchUtente(scelta);
+        Utente u = NULL;
+
+        if (scelta == 1) {
+            u = loginRegisterUtente(&tabUtenti);
+            if (u != NULL) {
+                do {
+                    sceltaMenu = menuUtente();
+                    listaPrenotazioni = switchUtente(sceltaMenu, u, listaPrenotazioni, tabAuto);
+                    system("pause");
+                    system("cls");
+                } while (sceltaMenu != 6);
+            }
+        } else if (scelta == 2) {
+          do {
+                sceltaMenu = mostraMenuDeveloper();
+                gestisciMenuDeveloper(sceltaMenu, &tabAuto, listaPrenotazioni);
                 system("pause");
                 system("cls");
-            } while (scelta != 6);
+            } while (sceltaMenu != 5);
         }
-    } else if (scelta == 2) {
-      do {
-            scelta = mostraMenuDeveloper();
-            gestisciMenuDeveloper(scelta, &tabAuto);
-            system("pause");
-            system("cls");
-        } while (scelta != 5);
-    }
+    } while (scelta != 0);
+
+    printf("Grazie per aver  Car Sharing.\n");
 
     // Alla fine distruggiamo la tabella
     distruggiHashTableUtenti(&tabUtenti);
