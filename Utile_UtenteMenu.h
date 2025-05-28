b08cec564a9bc5f68afdd35d4bcb6f5ee16fa4e2
G-Sarlo <sarlo.giuseppe@gmail.com>
2025-05-28 23:23:25 +0200

    Aggiornamento ai file Utile_UtenteMenu.c e Utile_UtenteMenu
    Correzioni ai file Utile_UtenteMenu.c e Utile_UtenteMenu; L'inserimento e prenotazioneAuto sono state rese due funzioni diverse.

--- Utile_UtenteMenu.h
+++ Utile_UtenteMenu.h
@@ -4,12 +4,15 @@
 #include "List_Prenotazione.h"
 #include "HashTbAuto.h"
 int codiceFiscaleValido(const char *CF);
-Lista prenotazioneAuto(Lista l, Utente u, AutoHashTB tabAuto) ;
+void visualizzaAutoDisponibiliConParametri(AutoHashTB ht, int giornoInizio, int giornoFine, int oraInizio, int oraFine);
+Lista prenotazioneAuto(Lista l, AutoHashTB tabAuto, char *CF, char *targa, int *giornoInizio, int *giornoFine, int *oraInizio, int *oraFine, int *stato);
+int inserimentoPrenotazione(char *CF, char *targa, int *giornoInizio, int *giornoFine, int *oraInizio, int *oraFine, Utente u);
 void visualizzaAutoDisponibili(AutoHashTB ht) ;
 void visualizzaPrenotazioniPerUtente(Lista l, const char *CF) ;
 void visualizzaStorico();
 Lista switchUtente(int scelta, Utente u, Lista l, AutoHashTB tabAuto);
 int menuUtente();
-void calcolaPrezziPrenotazioni(Lista prenotazioni, Utente u, AutoHashTB ht);
+float calcolaPrezziPrenotazioni(Lista prenotazioni, Utente u, AutoHashTB ht);
+float calcolaPrezzoFinale(Auto a, int giornoInizio, int giornoFine, int oraInizio, int oraFine);
 
 #endif //UTILE_UTENTEMENU_H
